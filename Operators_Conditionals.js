017f293b8716079bb864171ef7483cb710761d0f
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-09-28 23:57:17 +0530

    Basic

---
+++ Operators_Conditionals.js
@@ -0,0 +1,128 @@
+// OPERATORS 
+    // Arithmetic Operators : 
+
+            // Binary operations 
+            let  a =1;
+            let b=2;
+
+            // console.log(a+b);
+
+            // Unary Operations
+                // pre Increament -> ++a;
+                // posg Increament -> a++;
+
+
+                // console.log(a++);
+                // console.log(a);
+
+    // Comparison opertors
+
+            // >  
+            // <
+            // >=
+            // <=
+            // ==   -> loose Equality ( ony value should be equal)
+
+            // console.log("5"==5);
+            // console.log(5===5);
+            
+            
+            // ===  -> strict Equality ( both Value  && data type should be same )
+
+    
+    // ternary operators 
+
+            // syntax :  (condition) ? {true value} : {false value}
+
+            // let age =18;
+
+            // let CheckKaro=(age>=18) ? "I can vote " : "you can not vote "
+            // console.log(CheckKaro);
+
+
+    // Logical OPerator 
+
+            // AND -> &&    (if everything is true then only it will run or else nothing will execute )
+
+            // let ans=(true && false && true )
+            // console.log(ans);
+            
+            // OR -> ||     ( if any one condiditon is true the output is TRUE)
+
+            // let ans2=(false || false || true)
+            // console.log(ans2);
+            
+            // NOT -> !
+
+
+        // Working with Non  BOolean 
+
+            // some falsey condition : 
+                // undefined
+                // NULL
+                // 0
+                // false
+                // NaN
+
+            // some truth condition
+                // everything which is not falsey
+
+        
+    
+    // Bitwise Operators
+        // AND 
+        // OR 
+        // NOT
+        // <<  - right shift ->  shifting the binary value by a number 
+
+        // example : 
+        // 5 << 1   -> binary value of 5 : 0000101 
+        // after right shift -> 000001010 == 10 
+        // so the formnula is 5 << 1 : multiply 5 by 2 (1)
+        
+        
+
+        // >>
+        // XOR
+
+
+
+// conditional Statements ------------------------------------------------------------------------------------------------------
+
+// if else condition 
+// syntax : if(condiiton){}
+            // else{}
+
+
+// switch Case
+// syntax : 
+        // switch (expression){
+        //     case _ : output ;
+        //     case _ : output ;
+        //     case _ : output ;
+
+        // }
+
+let num=3;
+switch(num){
+    case 1: console.log('A'); break;
+    case 2: console.log('B'); break;
+    case 3: console.log('C'); break;
+    case 4: console.log('D'); break;
+    case 5: console.log('E'); break;
+    default: console.log("Maje karo");
+    
+    
+}
+
+
+
+            
+  
+        
+        
+        
+        
+
+    
+
