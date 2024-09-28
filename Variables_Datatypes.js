86e6b6e97659b2cbb02f987d6030716b0317342c
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-09-28 11:47:41 +0530

    Variable and Data types

---
+++ Variables_Datatypes.js
@@ -0,0 +1,66 @@
+// let age=19;
+// let naam="rachit"
+// console.log(age , naam);
+
+// Var is not used now because - It is not Block Scoped
+// reason codehelp ki JS documentation 
+
+// so we used Let - it is block Scoped
+
+
+// example of Block scopeed
+// {
+//     let a =12;
+//     console.log(a);
+    
+// }
+
+
+
+//  reasssigning and re difintion 
+
+// let x=12;
+// console.log(x);
+
+// x="rachit";
+// console.log(x);
+
+
+// // CONST
+// const a=28;
+// console.log(a);
+
+
+// let YourName="rachit"
+// console.log(YourName);
+
+
+
+// DATA TYPES ------------------------------------------------------------------------
+// primitive Data types : 
+        // -Number 
+        // -string 
+        // -boolean 
+        // -undefined
+        // -null 
+        // -big int 
+        // -Symbol
+
+
+// some fuctions : 
+// - typeof(); ->  checking the data type of a paticular variable 
+let naam="rachit dhaka "
+let age;
+let checking ="rachit";
+
+console.log(naam);
+console.log(age);
+console.log(typeof(checking));
+
+// Non Primitive data type 
+
+
+
+
+
+
