032ebc212e75336a2a1a8ce83ad12baeb5d365e8
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-09-29 22:27:51 +0530

    basic 2 3 hr

--- loops_string.js
+++ loops_string.js
@@ -1,23 +1,70 @@
-console.log("namaste duniya");
+// console.log("namaste duniya");
 
 
 
-for(let i=5;i>0;i--){
-    if(i==4){
-        continue
-    }
-    console.log(i);
+// for(let i=5;i>0;i--){
+//     if(i==4){
+//         continue
+//     }
+//     console.log(i);
 
-}
+// }
 
 
 
-// while loop
-let i=1
-while(i<=10){
-    console.log(i)
-    i++
-}
+// // while loop
+// let i=1
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+
+// string --------------------------------------------------------
+
+// string Operations 
+//     concatenation
+//     substring 
+//     length
+//     uppercase
+//     lowercase
+
+// let firstName=`hello ji 
+// namaste 
+// mera 
+// naame 
+// rachit 
+// hai `
+// let lastName="dhaka"
+
+// console.log(firstName)
+
+// let naam = new String(`rachit dhaka`);
+// console.log(naam);
+
+// let op1= `english `;
+// let op2= `hindi`;
+
+// let ans=op1+op2;
+// let finalAns=`${op1}${op2}`
+
+// console.log(op1.toLowerCase());
+
+
+// let str="abcdefg"
+// console.log(str.substring(0,5) )
+
+
+// let sentence="hello ji kese ho sare";
+// let work = sentence.split(' ');
+// console.log(work);
+// console.log(work.join(' '))
+
+
+
+
+
+
 
 
 
