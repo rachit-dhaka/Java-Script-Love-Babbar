c1ad95029bbfad6a577af4ebef7b6c4901d462bc
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-09-29 21:45:12 +0530

    basic 2

---
+++ loops_string.js
@@ -0,0 +1,25 @@
+console.log("namaste duniya");
+
+
+
+for(let i=5;i>0;i--){
+    if(i==4){
+        continue
+    }
+    console.log(i);
+
+}
+
+
+
+// while loop
+let i=1
+while(i<=10){
+    console.log(i)
+    i++
+}
+
+
+
+
+
