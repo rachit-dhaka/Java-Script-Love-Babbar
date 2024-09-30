d17613549d334da103aa27830397b2e217ee5b3b
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-09-30 23:55:07 +0530

    array  in js

---
+++ array.js
@@ -0,0 +1,22 @@
+// Refrenec type : 
+    // -> object 
+    // -> Array
+    // ->function
+
+
+// OBJECT CREATIONS 
+let obj = {
+    naam : "Rachit",
+    age: 20,
+    weight: 66,
+    height: "6ft"
+
+
+    greet:function(){
+        console.log("heloo ji");
+        
+    }
+
+};
+
+console.log(obj)
---
+++ functions.js
@@ -0,0 +1,32 @@
+// function SayMyName(){
+//     console.log("rachit dhaka");
+// }
+
+
+// SayMyName();
+
+// function PrintCounting(){
+//     for(let i=0;i<100;i++)
+//         console.log(i);
+// }
+
+// PrintCounting();
+
+
+
+// let getSum= function(a,b){
+//     return a+b;
+// }
+
+// console.log(getSum(2,3));
+
+
+
+// ARROW FUCITON
+
+// let GetSum = (a,b) =>{
+//     return a+b;
+// }
+
+// console.log(GetSum(2,3));
+
