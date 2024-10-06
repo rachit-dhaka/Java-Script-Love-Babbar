b20308eb0ca95caaf721a733c702f01c6537d05f
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-10-06 18:48:39 +0530

    dynamic nature of objects and cloning

--- object&functionality.js
+++ object&functionality.js
@@ -1,5 +1,39 @@
-let obj={
-    age:20,
-    wt:66,
-    ht:180
-};
+// let obj={
+//     age:20,
+//     wt:66,
+//     ht:180
+// };
+
+// console.log(obj);
+
+// obj.colour=`white`;
+// console.log(obj);
+
+
+// cloneing the object 
+
+    let src={
+        age: 20,
+        wt:66,
+        ht:180
+    };
+
+    let src2={
+        naam:`rachit`,
+
+    };
+
+    // using spread operator to clone 
+        // let des={...src};
+
+    // using assign method to clone a object 
+        // let des=Object.assign({},src,src2);
+
+    
+        
+    console.log("This is the SRC object : " ,src);
+    console.log("This is the DES object : " ,des);
+    
+
+
+
