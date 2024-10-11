ba9bdd315cd18b79afeef03306d2441ebbfed431
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-10-11 16:32:23 +0530

    dom manipulating html

---
+++ Js DOM/index.js
@@ -0,0 +1,11 @@
+let mydiv=document.querySelector('#mydiv');
+
+let newElement=document.createElement('span');
+newElement.textContent="love babbar ki line hai"
+
+mydiv.insertAdjacentElement('afterbegin',newElement);
+
+let parent=document.querySelector('#mydiv')
+let child=document.querySelector('#fh2')
+
+parent.removeChild(child);
