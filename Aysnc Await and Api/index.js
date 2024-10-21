97da829f0bfdcba59d0a17f15a3f8c64bec71564
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-10-22 00:10:50 +0530

    learnt about Async await function and Fetch API

---
+++ Aysnc Await and Api/index.js
@@ -0,0 +1,26 @@
+// async function getdata() {
+//     setTimeout(() => {
+//         console.log("me asysn fucntion ke andhar hu ");
+//     }, 3000);
+// }
+
+// getdata();
+
+
+// await 
+
+
+// async function something() {
+
+//     // get request - asyn code 
+//     let response= await fetch("https://jsonplaceholder.typicode.com/comments?postId=1");
+//     // async code 
+//     let data= await response.json();
+//     // sync code 
+//     console.log(response);
+        
+// }
+
+// something();
+
+
